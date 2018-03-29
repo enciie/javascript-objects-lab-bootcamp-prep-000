86b228e10d0a6e6d41c2a2a2617b454872d81303
Nancy Jo Chang <enciie@gmail.com>
2018-03-29 23:54:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,5 +7,6 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   return object
 }
 function deleteFromObjectByKey(object, key){
-  newObj = Object.assign({}, object)
+  var newObj = Object.assign({}, object)
+  newObj
 }
